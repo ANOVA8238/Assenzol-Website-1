e23ecf69444304ce6cd269e7f8715a9712dc387d
ANOVA8238 <stephen200008@gmail.com>
2026-02-26 23:49:54 +0800

    feat: Update project for Assenzol Lubricants
    Refactor project structure and dependencies for the Assenzol Lubricants website. This includes updating the project name, tsconfig, vite configuration, and removing unnecessary environment variables and metadata.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  base: '/Assenzol-Website-1/',
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    hmr: process.env.DISABLE_HMR !== 'true',
-  },
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
 });
